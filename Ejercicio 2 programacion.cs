35d356d59ef3d6e5eafd92fb4bfa980ec076398f
26STH01-SantiagoTurcios <turciossantiago16db@gmail.com>
2026-04-16 11:41:52 -0600

    Ejercicio 2 Programacion

---
+++ Ejercicio 2 programacion.cs
@@ -0,0 +1,50 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Escribir dos valores numericos: ");
+        double num1=Convert.ToDouble(Console.ReadLine());
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("\n *-*-*-*-* Menu Principal *-*-*-*-*");
+        Console.WriteLine("1- Suma");
+        Console.WriteLine("2- Resta");
+        Console.WriteLine("3- Multiplicacion");
+        Console.WriteLine("4- Division");
+        Console.WriteLine("5- Salir");
+        Console.WriteLine("Digite el valor segun sea la operacion: [ ]");
+        int opc=Convert.ToInt32(Console.ReadLine());
+        string msj = "";
+        double result=0;
+        switch (opc)
+        {
+            case 1:
+                msj = "\n SUMA \nla suma es: ";
+                result = num1 + num2;
+                break;
+            case 2:
+                msj = "\n RESTA \nla resta es: ";
+                result = num1 - num2;
+                break;
+            case 3:
+                msj = "\n Multiplicacion \nla multiplicacion es: ";
+                result = num1 * num2;
+                break;
+            case 4:
+                msj = "\n Division \nla division es: ";
+                result = num1 / num2;
+                break;
+            case 5:
+                msj = "Saliendo del sistema ";
+                break;
+            default:
+                msj = "dijite una opcion valida";
+                break;
+        }
+        Console.Clear();
+        Console.BackgroundColor = ConsoleColor.Green;
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(msj);
+        Console.WriteLine(result);
+    }
+}
